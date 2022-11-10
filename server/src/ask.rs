7cd01e40c2f9e2ae95a03c3e0fb9582e4f47b2f2
Jon Gjengset <jon@thesquareplanet.com>
2022-11-10 09:14:44 -0800

    Reject questions that are just URLs

--- server/src/ask.rs
+++ server/src/ask.rs
@@ -68,6 +68,11 @@ pub(super) async fn ask(
     body: String,
     Extension(dynamo): Extension<Backend>,
 ) -> Result<Json<serde_json::Value>, StatusCode> {
+    if http::Uri::try_from(body.trim()).is_ok() {
+        warn!(%eid, body, "rejecting URL-only question");
+        return Err(http::StatusCode::BAD_REQUEST);
+    }
+
     // TODO: check that eid actually exists
     // TODO: UUIDv7
     let qid = uuid::Uuid::new_v4();
