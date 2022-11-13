94759ef95cbdaec6550019f86113f41a37e034f8
Jon Gjengset <jon@thesquareplanet.com>
2022-11-12 16:35:22 -0800

    Add runner for tests with real DynamoDB

---
+++ server/run-tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+json=$(aws sts assume-role --role-arn "arn:aws:iam::880545379339:role/OrganizationAccountAccessRole" --role-session-name cargo-test)
+AWS_ACCESS_KEY_ID=$(jq -r .Credentials.AccessKeyId <<<"$json")
+AWS_SECRET_ACCESS_KEY=$(jq -r .Credentials.SecretAccessKey <<<"$json")
+AWS_SESSION_TOKEN=$(jq -r .Credentials.SessionToken <<<"$json")
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
+cargo t -- --ignored
