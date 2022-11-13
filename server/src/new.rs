ba3bd920c3e0a85a03fa3fab66b2422a112ad903
Jon Gjengset <jon@thesquareplanet.com>
2022-11-12 16:35:00 -0800

    Test new

--- server/src/new.rs
+++ server/src/new.rs
@@ -56,6 +56,52 @@ impl Backend {
             }
         }
     }
+
+    #[cfg(test)]
+    pub(super) async fn delete(&self, eid: &Uuid) {
+        let qs = self.list(eid, false).await.unwrap();
+        let qids: Vec<_> = qs
+            .items()
+            .into_iter()
+            .flat_map(|qs| qs.iter().filter_map(|doc| doc["id"].as_s().ok()))
+            .cloned()
+            .collect();
+
+        match self {
+            Self::Dynamo(dynamo) => {
+                for qid in qids {
+                    dynamo
+                        .delete_item()
+                        .table_name("questions")
+                        .key("id", AttributeValue::S(qid))
+                        .send()
+                        .await
+                        .unwrap();
+                }
+                dynamo
+                    .delete_item()
+                    .table_name("events")
+                    .key("id", AttributeValue::S(eid.to_string()))
+                    .send()
+                    .await
+                    .unwrap();
+            }
+            Self::Local(local) => {
+                let mut local = local.lock().unwrap();
+                let Local {
+                    events,
+                    questions,
+                    questions_by_eid,
+                    ..
+                } = &mut *local;
+
+                for qid in questions_by_eid.remove(&eid).unwrap() {
+                    questions.remove(&qid).unwrap();
+                }
+                events.remove(&eid).unwrap();
+            }
+        }
+    }
 }
 
 pub(super) async fn new(
@@ -79,5 +125,28 @@ pub(super) async fn new(
             error!(%eid, error = %e, "dynamodb request to create event failed");
             Err(http::StatusCode::INTERNAL_SERVER_ERROR)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    async fn inner(backend: Backend) {
+        let eid = Uuid::new_v4();
+        let secret = "cargo-test";
+        let _ = backend.new(&eid, secret).await.unwrap();
+        backend.delete(&eid).await;
+    }
+
+    #[tokio::test]
+    async fn local() {
+        inner(Backend::local().await).await;
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn dynamodb() {
+        inner(Backend::dynamo().await).await;
     }
 }
